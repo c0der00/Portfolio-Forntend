b98c52565979471c014e2caf2588741745e31f9d
c0der00 <darklucifer1951@gmail.com>
2025-06-16 10:02:45 +0530

    update fornend

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -21,7 +21,7 @@ const ContactForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8000/api/contact', formData);
+      const response = await axios.post('https://portfolio-backend-kappa-five.vercel.app/api/contact', formData);
       alert(response.data);
     } catch (error) {
       console.error('Error sending email:', error);
